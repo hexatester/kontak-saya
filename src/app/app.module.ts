6c51b326e190ba56652611705a2bb8a0760c1857
hexatester <revolusi147id@gmail.com>
2020-11-04 17:30:48 +0700

    improve import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ServiceWorkerModule } from '@angular/service-worker';
 import { StoreModule } from '@ngrx/store';
-import { reducers, metaReducers } from './reducers';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
@@ -13,10 +11,13 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatMenuModule } from '@angular/material/menu';
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { environment } from '../environments/environment';
-import { TentangComponent } from './tentang/tentang.component';
 import { MatCardModule } from '@angular/material/card';
+
+import { AppRoutingModule } from './app-routing.module';
+import { reducers, metaReducers } from './reducers';
+import { AppComponent } from './app.component';
+import { TentangComponent } from './tentang/tentang.component';
+import { environment } from '../environments/environment';
 
 const materialModules = [
   MatToolbarModule,
