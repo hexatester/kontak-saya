8036fb24100d8f01065199cc7e42312c30e92302
hexatester <revolusi147id@gmail.com>
2020-11-06 11:23:54 +0700

    refactor unused code

--- src/app/orang/orang-list/orang-list.component.ts
+++ src/app/orang/orang-list/orang-list.component.ts
@@ -37,49 +37,7 @@ export class OrangListComponent implements OnInit, AfterViewInit {
     this.table.dataSource = this.service;
   }
 
-  connect(): Observable<Array<Orang>> {
-    const dataMutations = [
-      this.orangs$,
-      this.paginator.page,
-      this.sort.sortChange,
-    ];
-    return merge(...dataMutations).pipe(
-      map(() => {
-        let orangs: Orang[];
-        this.orangs$.pipe(take(1)).subscribe((o) => (orangs = o));
-        return this.getPagedData(this.getSortedData([...orangs]));
-      })
-    );
-  }
-
-  private getPagedData(data: Orang[]) {
-    const startIndex = this.paginator.pageIndex * this.paginator.pageSize;
-    return data.splice(startIndex, this.paginator.pageSize);
-  }
-  private getSortedData(data: Orang[]) {
-    if (!this.sort.active || this.sort.direction === '') {
-      return data;
-    }
-
-    return data.sort((a, b) => {
-      const isAsc = this.sort.direction === 'asc';
-      switch (this.sort.active) {
-        case 'name':
-          return compare(a.nama, b.nama, isAsc);
-        case 'id':
-          return compare(+a.id, +b.id, isAsc);
-        default:
-          return 0;
-      }
-    });
-  }
-
   delete(id: string): void {
     this.store.dispatch(hapusOrang({ id }));
   }
-}
-
-/** Simple sort comparator for example ID/Name columns (for client-side sorting). */
-function compare(a: string | number, b: string | number, isAsc: boolean) {
-  return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
 }
