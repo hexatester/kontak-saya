b3bba9a248eb5b0b921948f65c501d4864a44468
hexatester <revolusi147id@gmail.com>
2020-11-15 10:36:52 +0700

    update @angular to v11

--- src/app/kontak/kontak-list/kontak-list.component.spec.ts
+++ src/app/kontak/kontak-list/kontak-list.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
@@ -10,7 +10,7 @@ describe('KontakListComponent', () => {
   let component: KontakListComponent;
   let fixture: ComponentFixture<KontakListComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ KontakListComponent ],
       imports: [
